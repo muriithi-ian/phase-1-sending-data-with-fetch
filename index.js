45c83707c8dafabfef46b67656e66e65448c5718
Muriithi Ian <imuriithiian@gmail.com>
2022-08-25 15:51:56 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+	return fetch("http://localhost:3000/users", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify({
+			name,
+			email,
+		}),
+	})
+		.then(function (res) {
+			return res.json();
+		})
+		.then(function (user) {
+			document.body.innerHTML = user["id"];
+		})
+		.catch(function (err) {
+			document.body.innerHTML = err.message;
+		});
+}
